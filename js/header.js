1fd902f826ae23f2d88b5a90c9de91185b65ecae
zubritckiy Ivan <doshick203@yandex.ru>
2022-04-27 00:43:40 +0300

    day-2 end

--- js/header.js
+++ js/header.js
@@ -10,3 +10,42 @@ for (let index = 0; index < changers.length; index++) {
     }
   }
 }
+
+
+
+const swiper = new Swiper('.swiper.students', {
+  // Default parameters
+  slidesPerView: 3,
+  spaceBetween: 64,
+  navigation: {
+    nextEl: ".students__navigation>.swiper-button-next",
+    prevEl: ".students__navigation>.swiper-button-prev",
+  },
+  /* работает, как media(min-width: ширина) */
+  breakpoints: {
+
+    1040: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+    720: {
+      slidesPerView: 2,
+      spaceBetween: 100
+    },
+    0: {
+      slidesPerView: 1,
+    }
+
+  }
+})
+
+const swiper1 = new Swiper('.swiper.offer__alpha-wrapper', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 64,
+  navigation: {
+    nextEl: ".offer__navigation>.swiper-button-next",
+    prevEl: ".offer__navigation>.swiper-button-prev",
+  },
+
+})
