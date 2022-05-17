0fb731b713c7b84829dfe2e7a9ced7da7a08f82a
zubritckiy Ivan <doshick203@yandex.ru>
2022-05-17 14:37:16 +0300

    test fix 1

--- js/pages/tests/moves.js
+++ js/pages/tests/moves.js
@@ -1,9 +1,19 @@
 const Moves = document.querySelector(".tests__moves")
 
-Moves.onclick = function () {
-  this.classList.toggle("opened")
-}
 
+let movesItem = document.querySelectorAll(".test__moves-item");
+for(let index = 0; index < movesItem.length; index++){
+  movesItem[index].onclick = function(){
+    if(Moves.classList.contains("opened")){
+      let currentItem = document.querySelector(".test__moves-item.current");
+      currentItem.classList.remove("current")
+      movesItem[index].classList.add("current")
+      Moves.classList.toggle("opened")
+    }else{
+      Moves.classList.toggle("opened")
+    }
+  }
+}
 
 
 
